eac7d7f4cddc652b0659d3ab7c8ad14766343e2a
Mohsin Ansari <mohsinansari@Meeshos-MacBook-Pro-2.local>
2021-02-11 13:46:26 +0530

    Updated error response

--- src/main/java/com/meesho/mohsin/NotificationService/model/response/ErrorMessageResponse.java
+++ src/main/java/com/meesho/mohsin/NotificationService/model/response/ErrorMessageResponse.java
@@ -1,4 +1,16 @@
 package com.meesho.mohsin.NotificationService.model.response;
 
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class ErrorMessageResponse {
+    private String code;
+    private String message;
+    public ErrorMessageResponse(){
+        code = "INVALID_REQUEST";
+        message = "phone_number is mandatory";
+    }
 }
